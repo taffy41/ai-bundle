symfony/ai

[Store] Add support for Redis

src/ai-bundle/config/options.php
@@ -15,6 +15,7 @@ use Codewithkyrian\ChromaDB\Client as ChromaDbClient;
 use MongoDB\Client as MongoDbClient;
 use Probots\Pinecone\Client as PineconeClient;
 use Symfony\AI\Platform\PlatformInterface;
+use Symfony\AI\Store\Bridge\Redis\Distance;
 use Symfony\AI\Store\StoreInterface;
 
 return static function (DefinitionConfigurator $configurator): void {
@@ -252,10 +253,30 @@ return static function (DefinitionConfigurator $configurator): void {
                             ->end()
                         ->end()
                     ->end()
+                    ->arrayNode('redis')
+                        ->normalizeKeys(false)
+                        ->useAttributeAsKey('name')
+                        ->arrayPrototype()
+                            ->children()
+                                ->variableNode('connection_parameters')
+                                    ->info('see https://github.com/phpredis/phpredis?tab=readme-ov-file#example-1')
+                                    ->isRequired()
+                                    ->cannotBeEmpty()
+                                ->end()
+                                ->scalarNode('index_name')->isRequired()->cannotBeEmpty()->end()
+                                ->scalarNode('key_prefix')->defaultValue('vector:')->end()
+                                ->enumNode('distance')
+                                    ->info('Distance metric to use for vector similarity search')
+                                    ->values(Distance::cases())
+                                    ->defaultValue(Distance::Cosine)
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
                     ->arrayNode('surreal_db')
                         ->normalizeKeys(false)
                         ->useAttributeAsKey('name')
-                            ->arrayPrototype()
+                        ->arrayPrototype()
                             ->children()
                                 ->scalarNode('endpoint')->cannotBeEmpty()->end()
                                 ->scalarNode('username')->cannotBeEmpty()->end()

taffy41/ai-bundle

config/options.php
@@ -17,6 +17,7 @@ use Probots\Pinecone\Client as PineconeClient;
 use Symfony\AI\Platform\Bridge\OpenAi\PlatformFactory;
 use Symfony\AI\Platform\Capability;
 use Symfony\AI\Platform\PlatformInterface;
+use Symfony\AI\Store\Bridge\Redis\Distance;
 use Symfony\AI\Store\Document\VectorizerInterface;
 use Symfony\AI\Store\StoreInterface;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
@@ -595,6 +596,36 @@ return static function (DefinitionConfigurator $configurator): void {
                                 ->stringNode('collection_name')->cannotBeEmpty()->end()
                                 ->integerNode('dimensions')->end()
                                 ->stringNode('distance')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                    ->arrayNode('redis')
+                        ->useAttributeAsKey('name')
+                        ->arrayPrototype()
+                            ->children()
+                                ->variableNode('connection_parameters')
+                                    ->info('see https://github.com/phpredis/phpredis?tab=readme-ov-file#example-1')
+                                    ->cannotBeEmpty()
+                                ->end()
+                                ->stringNode('client')
+                                    ->info('a service id of a Redis client')
+                                    ->cannotBeEmpty()
+                                ->end()
+                                ->stringNode('index_name')->isRequired()->cannotBeEmpty()->end()
+                                ->stringNode('key_prefix')->defaultValue('vector:')->end()
+                                ->enumNode('distance')
+                                    ->info('Distance metric to use for vector similarity search')
+                                    ->values(Distance::cases())
+                                    ->defaultValue(Distance::Cosine)
+                                ->end()
+                            ->end()
+                            ->validate()
+                                ->ifTrue(static fn ($v) => !isset($v['connection_parameters']) && !isset($v['client']))
+                                ->thenInvalid('Either "connection_parameters" or "client" must be configured.')
+                            ->end()
+                            ->validate()
+                                ->ifTrue(static fn ($v) => isset($v['connection_parameters']) && isset($v['client']))
+                                ->thenInvalid('Either "connection_parameters" or "client" can be configured, but not both.')
                             ->end()
                         ->end()
                     ->end()