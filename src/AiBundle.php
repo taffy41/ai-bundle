adb3d9895e2fe0225cda1345b590ee7551ef8cd9
Christopher Hertel <mail@christopher-hertel.de>
2025-12-12 18:10:54 +0100

    Decouple Platform Token Usage handling from Agent by introducing new interface

--- src/AiBundle.php
+++ src/AiBundle.php
@@ -910,28 +910,6 @@ final class AiBundle extends AbstractBundle
             }
         }
 
-        // TOKEN USAGE TRACKING
-        if ($config['track_token_usage'] ?? true) {
-            $platformServiceId = $config['platform'];
-
-            if ($container->hasAlias($platformServiceId)) {
-                $platformServiceId = (string) $container->getAlias($platformServiceId);
-            }
-
-            if (str_starts_with($platformServiceId, 'ai.platform.')) {
-                $platform = u($platformServiceId)->after('ai.platform.')->toString();
-
-                if (str_contains($platform, 'azure')) {
-                    $platform = 'azure';
-                }
-
-                if ($container->hasDefinition('ai.platform.token_usage_processor.'.$platform)) {
-                    $container->getDefinition('ai.platform.token_usage_processor.'.$platform)
-                        ->addTag('ai.agent.output_processor', ['agent' => $agentId, 'priority' => -30]);
-                }
-            }
-        }
-
         // SYSTEM PROMPT
         if (isset($config['prompt'])) {
             $includeTools = isset($config['prompt']['include_tools']) && $config['prompt']['include_tools'];
